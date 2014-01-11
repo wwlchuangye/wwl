9379fb6e2b37b1f3381cc9f04f8b5bd9e0aff930
 WenLong <wwlchuangye@sohu.com>
2014-01-11 14:05:48 +0800

    no message

---
+++ hello7.0/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  hello7.0
+//
+//  Created by  WenLong on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ hello7.0/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello7.0
+//
+//  Created by  WenLong on 13-11-5.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
