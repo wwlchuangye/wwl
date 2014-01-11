d69b8301ff31d74fef443c71c8ec0152e0faf2de
 WenLong <wwlchuangye@sohu.com>
2014-01-11 14:42:48 +0800

    no message

---
+++ hello7.0/aaabb.h
@@ -0,0 +1,13 @@
+//
+//  aaabb.h
+//  hello7.0
+//
+//  Created by  WenLong on 14-1-11.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface aaabb : NSObject
+
+@end
---
+++ hello7.0/aaabb.m
@@ -0,0 +1,13 @@
+//
+//  aaabb.m
+//  hello7.0
+//
+//  Created by  WenLong on 14-1-11.
+//
+//
+
+#import "aaabb.h"
+
+@implementation aaabb
+
+@end
